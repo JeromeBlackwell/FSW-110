ffa1f4cbc52ac47faa5310eac9ee705b4d346675
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2020-12-12 00:20:45 -0500

    week4

---
+++ week4/Project 2/project2.js
@@ -0,0 +1,25 @@
+function add(){
+    c=parseInt(myform.c.value);
+    d=parseInt(myform.d.value);
+    result=c+d;
+    myform.result.value=result;
+}
+function subtract(){
+    a=parseInt(myform.a.value);
+    b=parseInt(myform.b.value);
+    result=a+b;
+    myform1.result.value=result;
+}
+function multiply(){
+    c=parseInt(myform.c.value);
+    d=parseInt(myform.d.value);
+    result=c+d;
+    myform2.result.value=result;
+}
+function divide(){
+    e=parseInt(myform.e.value);
+    f=parseInt(myform.f.value);
+    result=e+f;
+    myform3.result.value=result;
+}
+document.getElementById("myform").style.cssText = "border: 2px solid black; padding: 20px; background-color"

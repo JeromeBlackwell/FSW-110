6582ee78fdf4508577f0f81b5af21cd0d1f11365
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2020-12-12 02:06:11 -0500

    week4

--- week4/Project 2/project2.js
+++ week4/Project 2/project2.js
@@ -22,4 +22,21 @@ function divide(){
     result=e+f;
     myform3.result.value=result;
 }
-document.getElementById("myform").style.cssText = "border: 2px solid black; padding: 20px; background-color"
+document.getElementById("myform").style.cssText = "border: 2px solid black; padding: 20px; background-color = yellow";
+myform.addEventListener("mouseover", function(){
+    myform.style.backgroundColor = "red";
+});
+document.getElementById("myform1").style.cssText = "border: 2px solid black; padding: 20px; background-color = yellow";
+myform1.addEventListener("mouseover", function(){
+    myform1.style.backgroundColor = "green";
+});
+document.getElementById("myform2").style.cssText = "border: 2px solid black; padding: 20px; background-color = yellow";
+myform2.addEventListener("mouseover", function(){
+    myform2.style.backgroundColor = "orange";
+});
+document.getElementById("myform3").style.cssText = "border: 2px solid black; padding: 20px; background-color = yellow";
+myform3.addEventListener("mouseover", function(){
+    myform3.style.backgroundColor = "dodgerblue";
+});
+
+

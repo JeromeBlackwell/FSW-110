15cb50bd427c5fa17eca48ceb43a48ad03fa81d3
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2020-12-13 03:50:30 -0500

    week4

--- week4/index.js
+++ week4/index.js
@@ -1,44 +1,47 @@
-    var h1 = document.createElement("h1")
-    h1.innterHTML = "";
-    document.body.append(h1);
-    h1.style.fontsize = "40px";
-    h1.style.fontweight = "lighter";
-    h1.style.fontfamily = "sans-serif";
-    h1.style.color = "cornflowerblue";
-    h1.style.textalign = "center";
-    h1.style.padding = "100px";
-    h1.style.border = "8px solid black";
-    h1.style.height = "300px";
-    h1.style.width = "250px";
+var h1 = document.createElement("h1")
+h1.innerText = "FSW-100 Week 4 Pause and Practice: Adding Event Listeners";
+document.body.append(h1);
+h1.style.fontsize = "40px";
+h1.style.fontweight = "lighter";
+h1.style.fontfamily = "sans-serif";
+h1.style.color = "cornflowerblue";
 
-h1.addEventListener("mouseover", function(){
-    h1.style.backgroundColor = "blue";
+var h2 = document.createElement("h2")
+h2.innterHTML = "";
+document.body.append(h2);
+h2.style.padding = "100px";
+h2.style.border = "8px solid black";
+h2.style.height = "300px";
+h2.style.width = "250px";
+
+h2.addEventListener("mouseover", function(){
+    h2.style.backgroundColor = "blue";
 });
 
-h1.addEventListener("mousedown", function(){
-    h1.style.backgroundColor = "red";
+h2.addEventListener("mousedown", function(){
+    h2.style.backgroundColor = "red";
 });
 
-h1.addEventListener("mouseup", function(){
-    h1.style.backgroundColor = "yellow";
+h2.addEventListener("mouseup", function(){
+    h2.style.backgroundColor = "yellow";
 });
 
-h1.addEventListener("dblclick", function(){
-    h1.style.backgroundColor = "green";
+h2.addEventListener("dblclick", function(){
+    h2.style.backgroundColor = "green";
 });
 
-h1.addEventListener("scroll", function(){
-    h1.style.backgroundColor = "orange";
+h2.addEventListener("scroll", function(){
+    h2.style.backgroundColor = "orange";
 });
 
 window.addEventListener("keydown", event => {
     if (event.key == "r"){
-    h1.style.backgroundColor = "red";
+    h2.style.backgroundColor = "red";
     }
 });
 
 window.addEventListener("keyup", event => {
     if (event.key == "r") {
-        h1.style.backgroundColor = "";
+        h2.style.backgroundColor = "";
     }
 });
